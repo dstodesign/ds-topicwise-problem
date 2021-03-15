4cffe8fadececdb994671052e04cf335201ba113
Vinayak Csk <47818838+cskvinayak@users.noreply.github.com>
2021-03-15 18:52:22 +0530

    Update LongestUniqueSubString.java

--- 1 sliding window/3 No-repeat Substring (hard)/LongestUniqueSubString.java
+++ 1 sliding window/3 No-repeat Substring (hard)/LongestUniqueSubString.java
@@ -20,4 +20,20 @@ class LongestUniqueSubString {
         }
         return Math.max(p2-p1, maxLen);
     }
+    
+    int longestUniqueSubstrWithHashMap(String S){
+        HashMap<Character, Integer> cache = new HashMap<>();
+        int maxLen = 0;
+        int p1=0, p2=0;
+        while(p1<=p2 && p2<S.length()) {
+            char c = S.charAt(p2);
+            if(cache.containsKey(c)) {
+                p1 = Math.max(p1, cache.get(c) + 1);
+            }
+            cache.put(c, p2);
+            p2++;
+            maxLen = Math.max(maxLen, p2-p1);
+        }
+        return maxLen;
+    }
 }
