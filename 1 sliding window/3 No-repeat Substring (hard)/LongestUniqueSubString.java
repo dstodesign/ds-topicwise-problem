850d4a326ba750cefd01f73a94459791db1e61eb
Vinayak Csk <47818838+cskvinayak@users.noreply.github.com>
2021-03-15 18:35:43 +0530

    Create LongestUniqueSubString.java

---
+++ 1 sliding window/3 No-repeat Substring (hard)/LongestUniqueSubString.java
@@ -0,0 +1,23 @@
+class LongestUniqueSubString {
+    int longestUniqueSubsttr(String S){
+        int p1 = 0;
+        int p2 = 0;
+        int maxLen = 0;
+        List<Character> list = new ArrayList<>();
+        while(p1<=p2 && p2< S.length()){
+            char c = S.charAt(p2);
+            if(list.contains(c)) {
+                maxLen = Math.max(maxLen, p2-p1);
+                while(S.charAt(p1) != c){
+                    list.remove(0);
+                    p1++;
+                }
+                list.remove(0);
+                p1++;
+            }
+            list.add(c);
+            p2++;
+        }
+        return Math.max(p2-p1, maxLen);
+    }
+}
